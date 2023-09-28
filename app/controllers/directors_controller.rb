5c5f25e61273e7fdac8d1b2772bb10e2ad073d7b
Carlos Castaneda <101298482+CarlosCastanedaDev@users.noreply.github.com>
2023-09-28 15:49:25 +0000

    update

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -12,4 +12,28 @@ class DirectorsController < ApplicationController
 
     render({ :template => "director_templates/show"})
   end
+
+  def eldest
+    matching_record = Director.all.order({ :dob => :asc })
+
+    @eldest_name = matching_record.first.name
+    @eldest_id = matching_record.first.id
+    eldest_age = matching_record.first.dob
+    @eldest_date = eldest_age.strftime("%B %d, %Y")
+    # @eldest_date = eldest_age.nil? ? "error" : eldest_age.strftime("%B %d, %Y")
+
+    render({ :template => "director_templates/eldest"})
+  end
+
+  def youngest
+    matching_record = Director.all.order({ :dob => :asc })
+
+    @youngest_name = matching_record.last.name
+    @youngest_id = matching_record.last.id
+    youngest_age = matching_record.last.dob
+    @youngest_date = youngest_age.strftime("%B %d, %Y")
+
+    render({ :template => "director_templates/youngest"})
+  end
+
 end
